6597b4c78cff2ac942f6cae367ad70edddb06b4f
Nada-diaelsaid <nada_diaelsaid@hotmail.com>
2016-02-25 19:06:53 +0200

    countdown timer, labels backcolor

--- karate calculator/Form1.cs
+++ karate calculator/Form1.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing.Text;
-
+using System.Runtime;
 namespace karate_calculator
 {
+    
     public partial class Form1 : Form
     {
+        int min = 0, sec = 0;
+        //timer1.Tick += new EventHandler(timer1_Tick);
         [System.Runtime.InteropServices.DllImport("gdi32.dll")]
         private static extern IntPtr AddFontMemResourceEx(IntPtr pbFont, uint cbFont,
             IntPtr pdv, [System.Runtime.InteropServices.In] ref uint pcFonts);
@@ -123,6 +126,9 @@ namespace karate_calculator
         {
             redScore.Font =myFont;
             blueScore.Font = myFont;
+            minLabel.Font = myFont;
+            secLabel.Font = myFont;
+            dot.Font = myFont;
             ToolTip toolTip1 = new ToolTip();
 
             // Set up the delays for the ToolTip.
@@ -217,6 +223,332 @@ namespace karate_calculator
                 redScore.Text = s;
             }
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            
+            //timer1.Enabled = true;
+            
+            string temp;
+            /////put the seconds in the secLabel
+            temp = string.Format("{0:00}", sec);
+            secLabel.Text = temp;
+
+            /////put the minuits in the minLabel
+            minLabel.Text = min.ToString();
+
+            ///begin counting down
+            if (sec != 0) { 
+                sec--;
+            }
+            ///put it in the digital format
+            temp = string.Format("{0:00}", sec);
+            //put it again in secLabel
+            secLabel.Text = temp;
+            if (sec == 0 && min > 0) {
+                
+                    min--;
+                    minLabel.Text = min.ToString();
+                    sec = 60;
+                    temp = string.Format("{0:00}", sec);
+                    secLabel.Text = temp;
+                    ///timer1.Enabled = true;
+                    //timer1_Tick( sender,  e);
+            }
+            else if (sec == 0 && min == 0) { timer1.Stop(); }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            min = (int)numericUpDown1.Value;
+            sec = (int)numericUpDown2.Value;
+            if (min == 0 && sec == 0) { 
+                //do nothing
+            }
+            else
+              timer1.Start();
+        }
+
+        private void yukoB_Click(object sender, EventArgs e)
+        {
+            int temp;
+            temp = Int32.Parse(blueScore.Text);
+            temp = temp + 1;
+            /////format string in 00 form
+            string s;
+            s = string.Format("{0:00}", temp);
+            blueScore.Text = s;
+        }
+
+        private void wazariB_Click(object sender, EventArgs e)
+        {
+            int temp;
+            temp = Int32.Parse(blueScore.Text);
+            temp = temp + 2;
+            /////format string in 00 form
+            string s;
+            s = string.Format("{0:00}", temp);
+            blueScore.Text = s;
+        }
+
+        private void ipponB_Click(object sender, EventArgs e)
+        {
+            int temp;
+            temp = Int32.Parse(blueScore.Text);
+            temp = temp + 3;
+            /////format string in 00 form
+            string s;
+            s = string.Format("{0:00}", temp);
+            blueScore.Text = s;
+        }
+
+        private void yukoR_Click(object sender, EventArgs e)
+        {
+            int temp;
+            temp = Int32.Parse(redScore.Text);
+            temp = temp + 1;
+            /////format string in 00 form
+            string s;
+            s = string.Format("{0:00}", temp);
+            redScore.Text = s;
+        }
+
+        private void wazariR_Click(object sender, EventArgs e)
+        {
+            int temp;
+            temp = Int32.Parse(redScore.Text);
+            temp = temp + 2;
+            /////format string in 00 form
+            string s;
+            s = string.Format("{0:00}", temp);
+            redScore.Text = s;
+        }
+
+        private void ipponR_Click(object sender, EventArgs e)
+        {
+            int temp;
+            temp = Int32.Parse(redScore.Text);
+            temp = temp + 3;
+            /////format string in 00 form
+            string s;
+            s = string.Format("{0:00}", temp);
+            redScore.Text = s;
+        }
+
+        private void CBC1B_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBC1B.Checked == true) {
+                LC1B.BackColor = Color.Gray;
+            }
+            else if (CBC1B.Checked == false) {
+                LC1B.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBC2B_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBC2B.Checked == true)
+            {
+                LC2B.BackColor = Color.Gray;
+            }
+            else if (CBC2B.Checked == false)
+            {
+                LC2B.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBK1B_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBK1B.Checked == true)
+            {
+                LK1B.BackColor = Color.Gray;
+            }
+            else if (CBK1B.Checked == false)
+            {
+                LK1B.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBK2B_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBK2B.Checked == true)
+            {
+                LK2B.BackColor = Color.Gray;
+            }
+            else if (CBK2B.Checked == false)
+            {
+                LK2B.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBHC1B_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBHC1B.Checked == true)
+            {
+                LHC1B.BackColor = Color.Gray;
+            }
+            else if (CBHC1B.Checked == false)
+            {
+                LHC1B.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBHC2B_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBHC2B.Checked == true)
+            {
+                LHC2B.BackColor = Color.Gray;
+            }
+            else if (CBHC2B.Checked == false)
+            {
+                LHC2B.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBH1B_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBH1B.Checked == true)
+            {
+                LH1B.BackColor = Color.Gray;
+            }
+            else if (CBH1B.Checked == false)
+            {
+                LH1B.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBH2B_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBH2B.Checked == true)
+            {
+                LH2B.BackColor = Color.Gray;
+            }
+            else if (CBH2B.Checked == false)
+            {
+                LH2B.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBC1R_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBC1R.Checked == true)
+            {
+                LC1R.BackColor = Color.Gray;
+            }
+            else if (CBC1R.Checked == false)
+            {
+                LC1R.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBC2R_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBC2R.Checked == true)
+            {
+                LC2R.BackColor = Color.Gray;
+            }
+            else if (CBC2R.Checked == false)
+            {
+                LC2R.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBK1R_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBK1R.Checked == true)
+            {
+                LK1R.BackColor = Color.Gray;
+            }
+            else if (CBK1R.Checked == false)
+            {
+                LK1R.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBK2R_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBK2R.Checked == true)
+            {
+                LK2R.BackColor = Color.Gray;
+            }
+            else if (CBK2R.Checked == false)
+            {
+                LK2R.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBHC1R_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBHC1R.Checked == true)
+            {
+                LHC1R.BackColor = Color.Gray;
+            }
+            else if (CBHC1R.Checked == false)
+            {
+                LHC1R.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBHC2R_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBHC2R.Checked == true)
+            {
+                LHC2R.BackColor = Color.Gray;
+            }
+            else if (CBHC2R.Checked == false)
+            {
+                LHC2R.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBH1R_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBH1R.Checked == true)
+            {
+                LH1R.BackColor = Color.Gray;
+            }
+            else if (CBH1R.Checked == false)
+            {
+                LH1R.BackColor = Color.Transparent;
+            }
+        }
+
+        private void CBH2R_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBH2R.Checked == true)
+            {
+                LH2R.BackColor = Color.Gray;
+            }
+            else if (CBH2R.Checked == false)
+            {
+                LH2R.BackColor = Color.Transparent;
+            }
+        }
+
+        
+
+
+        /////////////RED CHECKBOXES
+        
+
+
+        
+        //private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        //{
+        //     minLabel.Text = numericUpDown1.Value.ToString();
+        //     secLabel.Text = numericUpDown2.Value.ToString();
+        //}
        
     }
 }
